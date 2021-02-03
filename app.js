807d8fb99f7e7251f8ffea22226ca9e75aad3045
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-02-03 22:53:08 +0600

    weather app updated

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const https = require('https')
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
-app.listen(port, () => console.log(`Example app listening on port port!`))
+const app = express()
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function (req, res) {
+    const query = req.body.cityName;
+    const apiKey = "1a301874a116ded8ea06640d46bfd1f4";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=" + query + "&units=" + unit;
+
+    https.get(url, function (respond) {
+        console.log(respond.statusCode);
+        respond.on("data", function (data) {
+            const weatherData = JSON.parse(data)
+            const temp = weatherData.main.temp
+            const weatherDescription = weatherData.weather[0].description
+            const icon = weatherData.weather[0].icon
+            const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+            res.write("<p>The weather is " + weatherDescription + " today </p>")
+            res.write("<h1>The temperature in " + query + " is " + temp + " degree celcius.</h1>")
+            res.write("<img src=" + imageUrl + ">");
+            res.send()
+            console.log(respond.statusCode);
+        })
+    })
+
+})
+
+app.listen(3000, () => console.log('Server is running on port 3000'))
